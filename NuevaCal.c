1d7bccea3279f85eee83f9ac460417c24901121d
alfonsodeabiega <alfonso.deabiega@gmail.com>
2024-09-27 15:01:19 -0600

    Create NuevaCal.c

---
+++ NuevaCal.c
@@ -0,0 +1,73 @@
+/*Alfonso Francisco De Abiega L'eglisse
+ejercicio 4 Calculadora con funciones
+Practica 7 27/09/24*/
+
+#include <stdio.h>
+#include <math.h>
+
+int resp;
+
+int suma(int a, int b)
+{
+  resp = a + b;
+  return(resp);
+}
+int resta(int a, int b)
+{
+  resp = a - b;
+  return (resp);
+}
+int mult(int a, int b)
+{
+  resp = a * b;
+  return (resp);
+}
+int div(int a, int b)
+{
+  resp = a / b;
+  return(resp);
+}
+int exp(int a, int b)
+{
+  resp = pow(a,b);
+  return(resp);
+}
+
+int main()
+{
+  int a = 0, b = 0;
+  int respuesta, opcion;
+
+  printf("Dime que operacion deseas realizar: \n");
+  printf("1 .- Suma\n");
+  printf("2 .- Resta\n");
+  printf("3 .- Multiplicacion\n");
+  printf("4 .- Division\n");
+  printf("5 .- Exponente\n");
+  scanf("%d", &option);
+
+  switch(opcion)
+  {
+    default:
+      respuesta = 0;
+      break;
+    case 1:
+      respuesta = suma(a,b);
+      break;
+    case 2:
+      respuesta = resta(a,b);
+      break;
+    case 3:
+      respuesta = mult(a,b);
+      break;
+    case 4:
+      respuesta = div(a,b);
+      break;
+    case 5:
+      respuesta = exp(a,b);
+      break;
+  }
+
+  printf("El resultado de la operacion es: %d\n", respuesta);
+  return 0;
+}
